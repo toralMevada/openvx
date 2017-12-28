037f2e9aad5841da4dd124f18b64a2d7b293853b
toralMevada <toralpanchal23@gmail.com>
2017-12-28 18:45:59 +0530

    updated main.c, corrected indexing

--- openvx_example/main.c
+++ openvx_example/main.c
@@ -3,8 +3,9 @@ int main{
 
 		int array[5]=0;
 		print("first elemnt of array is %d",*array);
-		print("first elemnt of array is %d",array[1]);
-		print("thiurd elemnt of array is %d",array[3]);
+		print("secong element of array is %d",array[1]);
+		print("third element of array is %d",array[2]);
+		print("forth element of array is %d",array[3]);
 		function_2();
 		function_1();
 	
